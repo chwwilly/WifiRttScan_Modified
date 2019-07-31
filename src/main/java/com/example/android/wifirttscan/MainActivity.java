47653a3bce6ad3c9123eaddf99f7930604624bbe
chwwilly <chwwilly@ntu.edu.tw>
2019-07-31 22:02:39 +0800

    Revert "Ver 1.1.1: Ranging request font change, Change icon"
    This reverts commit 7db87cd8

--- src/main/java/com/example/android/wifirttscan/MainActivity.java
+++ src/main/java/com/example/android/wifirttscan/MainActivity.java
@@ -179,6 +179,7 @@ public class MainActivity extends AppCompatActivity implements ScanResultClickLi
                                     + " RTT capable.");
 
                 } else {
+                    // TODO (jewalker): Add Snackbar regarding permissions
                     Log.d(TAG, "Permissions not allowed.");
                 }
             }
